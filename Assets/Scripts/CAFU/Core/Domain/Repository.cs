d99020539cf9afc9c58cbe7e41516b995a826bb8
Tetsuya Mori <monry84@gmail.com>
2017-12-04 15:45:16 +0900

    :sparkles: Implement factory class for Presenter and Repository
    * Presenter と Repository 用のファクトリクラスを実装

--- Assets/Scripts/CAFU/Core/Domain/Repository.cs
+++ Assets/Scripts/CAFU/Core/Domain/Repository.cs
@@ -4,4 +4,23 @@ namespace CAFU.Core.Domain {
     public interface IRepository {
     }
 
+    public interface IRepositoryBuilder {
+
+        void Build();
+
+    }
+
+    public static class RepositoryFactory {
+
+        public static T CreateInstance<T>() where T : IRepository, new() {
+            T instance = new T();
+            IRepositoryBuilder builder = instance as IRepositoryBuilder;
+            if (builder != default(IRepositoryBuilder)) {
+                builder.Build();
+            }
+            return instance;
+        }
+
+    }
+
 }
