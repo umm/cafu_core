62a29be502c40ddaed6c77a7ef54927a6da9b361
Tetsuya Mori <monry84@gmail.com>
2017-12-20 12:20:42 +0900

    :sparkles: Improve ViewExtension
    * Instantiate だと本来的な意味から外れてしまうため、意味を「View の生成」に限定する

--- Assets/Scripts/CAFU/Core/Presentation/View.cs
+++ Assets/Scripts/CAFU/Core/Presentation/View.cs
@@ -1,6 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using CAFU.Core.Domain;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace CAFU.Core.Presentation.View {
 
@@ -23,36 +25,84 @@ namespace CAFU.Core.Presentation.View {
 
     public static class ViewExtension {
 
+        public static IView AddChildView(this Transform transform, GameObject prefab) {
+            return transform.AddChildView<IView>(prefab);
+        }
+
+        public static IView AddChildView<TModel>(this Transform transform, GameObject prefab, TModel model)
+            where TModel : IModel {
+            return transform.AddChildView<IView, TModel>(prefab, model);
+        }
+
+        public static TView AddChildView<TView>(this Transform transform, GameObject prefab)
+            where TView : IView {
+            GameObject child = Object.Instantiate(prefab, transform);
+            TView childView = child.gameObject.GetComponent<TView>();
+            if (childView == null) {
+                throw new InvalidOperationException(string.Format("GameObject '{0}' has not attached component '{1}'.", child.name, typeof(TView).FullName));
+            }
+            return childView;
+        }
+
+        public static TView AddChildView<TView, TModel>(this Transform transform, GameObject prefab, TModel model)
+            where TView : IView
+            where TModel : IModel {
+            TView childView = transform.AddChildView<TView>(prefab);
+            MonoBehaviour childMonoBehaviour = childView as MonoBehaviour;
+            if (childMonoBehaviour == default(MonoBehaviour)) {
+                throw new InvalidOperationException(string.Format("'{0}' is not inheritance MonoBehaviour.", typeof(TView).FullName));
+            }
+            // Model 不要の IViewBuilder.Build() をコール
+            childMonoBehaviour.gameObject.GetComponents<IViewBuilder>().ToList().ForEach(
+                (x) => {
+                    x.Build();
+                }
+            );
+            if (model != null) {
+                // Model を要する IViewBuilder<TModel>.Build(TModel model) をコール
+                childMonoBehaviour.gameObject.GetComponents<IViewBuilder<TModel>>().ToList().ForEach(
+                    (x) => {
+                        x.Build(model);
+                    }
+                );
+            }
+            return childView;
+        }
+
+        [Obsolete("Please use `AddChildView()` instead of `InstantiateChild()`.")]
         public static IView InstantiateChild(this IView self, GameObject prefab) {
             return self.InstantiateChild<IView>(prefab);
         }
 
+        [Obsolete("Please use `AddChildView()` instead of `InstantiateChild()`.")]
         public static IView InstantiateChild<TModel>(this IView self, GameObject prefab, TModel model)
             where TModel : IModel {
             return self.InstantiateChild<IView, TModel>(prefab, model);
         }
 
-        private static TView InstantiateChild<TView>(this IView self, GameObject prefab)
+        [Obsolete("Please use `AddChildView()` instead of `InstantiateChild()`.")]
+        public static TView InstantiateChild<TView>(this IView self, GameObject prefab)
             where TView : IView {
             MonoBehaviour monoBehaviour = self as MonoBehaviour;
             if (monoBehaviour == default(MonoBehaviour)) {
-                throw new System.InvalidOperationException(string.Format("'{0}' is not inheritance MonoBehaviour.", typeof(TView).FullName));
+                throw new InvalidOperationException(string.Format("'{0}' is not inheritance MonoBehaviour.", typeof(TView).FullName));
             }
             GameObject child = Object.Instantiate(prefab, monoBehaviour.transform);
             TView childView = child.gameObject.GetComponent<TView>();
             if (childView == null) {
-                throw new System.InvalidOperationException(string.Format("GameObject '{0}' has not attached component '{1}'.", child.name, typeof(TView).FullName));
+                throw new InvalidOperationException(string.Format("GameObject '{0}' has not attached component '{1}'.", child.name, typeof(TView).FullName));
             }
             return childView;
         }
 
-        private static TView InstantiateChild<TView, TModel>(this IView self, GameObject prefab, TModel model)
+        [Obsolete("Please use `AddChildView()` instead of `InstantiateChild()`.")]
+        public static TView InstantiateChild<TView, TModel>(this IView self, GameObject prefab, TModel model, Transform parent = null)
             where TView : IView
             where TModel : IModel {
             TView childView = self.InstantiateChild<TView>(prefab);
             MonoBehaviour monoBehaviour = childView as MonoBehaviour;
             if (monoBehaviour == default(MonoBehaviour)) {
-                throw new System.InvalidOperationException(string.Format("'{0}' is not inheritance MonoBehaviour.", typeof(TView).FullName));
+                throw new InvalidOperationException(string.Format("'{0}' is not inheritance MonoBehaviour.", typeof(TView).FullName));
             }
             // Model 不要の IViewBuilder.Build() をコール
             monoBehaviour.gameObject.GetComponents<IViewBuilder>().ToList().ForEach(
