f743fa4e77ce744a97ecb01cdb2a691798bcd6c5
Tetsuya Mori <monry84@gmail.com>
2018-01-29 00:43:16 +0900

    :sparkles: Implement Factory class for Repository

--- Assets/Scripts/CAFU/Core/Domain/Repository.cs
+++ Assets/Scripts/CAFU/Core/Domain/Repository.cs
@@ -1,19 +1,38 @@
-﻿
+﻿using System;
+// ReSharper disable UnusedMember.Global
+
 namespace CAFU.Core.Domain {
 
     public interface IRepository {
     }
 
+    public interface IRepositoryFactory<out TRepository> where TRepository : IRepository {
+
+        TRepository Factory();
+
+    }
+
+    public class DefaultRepositoryFactory<TRepository> : IRepositoryFactory<TRepository> where TRepository : IRepository, new() {
+
+        public TRepository Factory() {
+            return new TRepository();
+        }
+
+    }
+
+    [Obsolete("Please use IRepositoryFactory<TUseCase> instead of this interface.")]
     public interface IRepositoryBuilder {
 
         void Build();
 
     }
 
+    [Obsolete("Please use DefaultRepositoryFactory instead of this class.")]
     public static class RepositoryFactory {
 
         public static T CreateInstance<T>() where T : IRepository, new() {
             T instance = new T();
+            // ReSharper disable once SuspiciousTypeConversion.Global
             IRepositoryBuilder builder = instance as IRepositoryBuilder;
             if (builder != default(IRepositoryBuilder)) {
                 builder.Build();
