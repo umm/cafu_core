45989b0c2cad5ce605c83cec87fcf54dbad81395
Tetsuya Mori <monry84@gmail.com>
2018-01-29 09:17:28 +0900

    :sparkles: Implement base factory class

---
+++ Assets/Scripts/CAFU/Core/Utility/Factory.cs
@@ -0,0 +1,30 @@
+﻿namespace CAFU.Core.Utility {
+
+    public class DefaultFactory<T> where T : new() {
+
+        public T Create() {
+            return new T();
+        }
+
+    }
+
+    public class DefaultSingletonFactory<T> where T : new() {
+
+        private static T instance;
+
+        private static T Instance {
+            get {
+                if (instance == null) {
+                    instance = new T();
+                }
+                return instance;
+            }
+        }
+
+        public T Create() {
+            return Instance;
+        }
+
+    }
+
+}
