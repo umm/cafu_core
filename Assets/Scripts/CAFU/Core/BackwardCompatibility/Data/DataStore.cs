37d196b90b9ced0c54a4c4a4ed53e744a635b31b
Tetsuya Mori <monry84@gmail.com>
2018-02-07 12:36:22 +0900

    :memo: Fix Obsolete messages

--- Assets/Scripts/CAFU/Core/BackwardCompatibility/Data/DataStore.cs
+++ Assets/Scripts/CAFU/Core/BackwardCompatibility/Data/DataStore.cs
@@ -10,22 +10,22 @@ namespace CAFU.Core.Data {
     }
 
     // ReSharper disable once UnusedTypeParameter
-    [Obsolete("Please use instead of this.")]
+    [Obsolete("Please use CAFU.Core.Data.DataStore.IDataStore instead of this.")]
     public interface IScriptableObjectDataStoreHandler<T> where T : ScriptableObject {
 
         string Path { get; }
 
     }
 
-    [Obsolete("Please use instead of this.")]
+    [Obsolete("Please use CAFU.Core.Data.DataStore.IDataStore instead of this.")]
     public interface IScriptableObjectDataStoreHandlerInScene<T> : IScriptableObjectDataStoreHandler<T> where T : ScriptableObject {
     }
 
-    [Obsolete("Please use instead of this.")]
+    [Obsolete("Please use CAFU.Core.Data.DataStore.IDataStore instead of this.")]
     public interface IScriptableObjectDataStoreHandlerInResources<T> : IScriptableObjectDataStoreHandler<T> where T : ScriptableObject {
     }
 
-    [Obsolete("Please use instead of this.")]
+    [Obsolete("Please use CAFU.Core.Data.DataStore.IDataStore instead of this.")]
     public interface IScriptableObjectDataStoreHandlerInStreamingAssets<T> : IScriptableObjectDataStoreHandler<T> where T : ScriptableObject {
     }
 
@@ -33,17 +33,17 @@ namespace CAFU.Core.Data {
 
         private const string EXTENSION = ".asset";
 
-        [Obsolete("Please use instead of this.")]
+        [Obsolete("Please use CAFU.Core.Data.Entity.ScriptableObject.Generator instead of this.")]
         public static string CreatePathInRuntime<T>(this IScriptableObjectDataStoreHandlerInResources<T> self) where T : ScriptableObject {
             return Regex.Replace(self.CreatePath(), string.Format("{0}$", EXTENSION), string.Empty);
         }
 
-        [Obsolete("Please use instead of this.")]
+        [Obsolete("Please use CAFU.Core.Data.Entity.ScriptableObject.Generator instead of this.")]
         public static string CreatePathInRuntime<T>(this IScriptableObjectDataStoreHandlerInStreamingAssets<T> self) where T : ScriptableObject {
             return Path.Combine(Application.streamingAssetsPath, self.CreatePath());
         }
 
-        [Obsolete("Please use instead of this.")]
+        [Obsolete("Please use CAFU.Core.Data.Entity.ScriptableObject.Generator instead of this.")]
         private static string CreatePath<T>(this IScriptableObjectDataStoreHandler<T> self) where T : ScriptableObject {
             if (Regex.IsMatch(self.Path, string.Format("{0}$", EXTENSION))) {
                 return self.Path;
@@ -52,22 +52,22 @@ namespace CAFU.Core.Data {
         }
 
 #if UNITY_EDITOR
-        [Obsolete("Please use instead of this.")]
+        [Obsolete("Please use CAFU.Core.Data.Entity.ScriptableObject.Generator instead of this.")]
         public static void CreateScriptableObjectAsset<T>(this IScriptableObjectDataStoreHandlerInScene<T> self) where T : ScriptableObject {
             CreateScriptableObjectAsset<T>(Path.Combine(Application.dataPath, self.CreatePath()));
         }
 
-        [Obsolete("Please use instead of this.")]
+        [Obsolete("Please use CAFU.Core.Data.Entity.ScriptableObject.Generator instead of this.")]
         public static void CreateScriptableObjectAsset<T>(this IScriptableObjectDataStoreHandlerInResources<T> self) where T : ScriptableObject {
             CreateScriptableObjectAsset<T>(Path.Combine(Path.Combine(Application.dataPath, "Resources"), self.CreatePath()));
         }
 
-        [Obsolete("Please use instead of this.")]
+        [Obsolete("Please use CAFU.Core.Data.Entity.ScriptableObject.Generator instead of this.")]
         public static void CreateScriptableObjectAsset<T>(this IScriptableObjectDataStoreHandlerInStreamingAssets<T> self) where T : ScriptableObject {
             CreateScriptableObjectAsset<T>(Path.Combine(Path.Combine(Application.dataPath, "StreamingAssets"), self.CreatePath()));
         }
 
-        [Obsolete("Please use instead of this.")]
+        [Obsolete("Please use CAFU.Core.Data.Entity.ScriptableObject.Generator instead of this.")]
         private static void CreateScriptableObjectAsset<T>(string fullPath) where T : ScriptableObject {
             if (File.Exists(fullPath)) {
                 Debug.LogWarningFormat("ScriptableObject for type of '{0}' is already exists in '{1}'.", typeof(T).FullName, fullPath);
